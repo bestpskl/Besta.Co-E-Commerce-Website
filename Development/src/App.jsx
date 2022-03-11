40f7e7dcd12a7a55312d83adebab1357ce4567cc
Pasakorn Limchuchua <66000440+bestpskl@users.noreply.github.com>
2022-03-11 21:30:34 +0700

    Add development files

---
+++ Development/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import SingleProduct from "./pages/SingleProduct";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ShoppingCart from "./pages/ShoppingCart";
+
+const App = () => {
+  return <ShoppingCart />;
+};
+
+export default App;
---
+++ Development/src/data.js
@@ -0,0 +1,93 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "./assets/fashion-slider-1.png",
+    title: "POPULAR SALE",
+    desc: "DON’T COMPROMISE ON STYLE GET FLAT 25% OFF FOR NEW CUSTOMERS.",
+    bg: "EEF7FF",
+  },
+  {
+    id: 2,
+    img: "./assets/fashion-slider-2.png",
+    title: "MATCH & STYLE",
+    desc: "DON’T COMPROMISE ON STYLE GET FLAT 25% OFF FOR NEW CUSTOMERS.",
+    bg: "FFEEFD",
+  },
+  {
+    id: 3,
+    img: "./assets/fashion-slider-3.png",
+    title: "SUMMER SALE",
+    desc: "DON’T COMPROMISE ON STYLE GET FLAT 25% OFF FOR NEW CUSTOMERS.",
+    bg: "FFFEEE",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "./assets/category-1.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 2,
+    img: "./assets/category-2.jpg",
+    title: "SHIRT LOOK",
+  },
+  {
+    id: 3,
+    img: "./assets/category-3.jpg",
+    title: "JACKET STYLES!",
+  },
+];
+
+
+export const listProducts = [
+  {
+    id:1,
+    img:"./assets/list-product-1-1.png",
+  },
+  {
+    id:2,
+    img:"./assets/list-product-1-2.png",
+  },
+  {
+    id:3,
+    img:"./assets/list-product-1-3.png",
+  },
+  {
+    id:4,
+    img:"./assets/list-product-1-4.png",
+  },
+  {
+    id:5,
+    img:"./assets/list-product-2-1.png",
+  },
+  {
+    id:6,
+    img:"./assets/list-product-2-2.png",
+  },
+  {
+    id:7,
+    img:"./assets/list-product-2-3.png",
+  },
+  {
+    id:8,
+    img:"./assets/list-product-2-4.png",
+  },
+  {
+    id:9,
+    img:"./assets/list-product-3-1.png",
+  },
+  {
+    id:10,
+    img:"./assets/list-product-3-2.png",
+  },
+  {
+    id:11,
+    img:"./assets/list-product-3-3.png",
+  },
+  {
+    id:12,
+    img:"./assets/list-product-3-4.png",
+  },  
+]
